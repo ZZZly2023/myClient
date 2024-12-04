8cebe8e2af78e79031be8f104f231b8d61173ef0
zhangly <zhanglys@yonyou.com>
2024-12-04 14:58:25 +0800

    [feature: 项目基础环境搭建]

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function App() {
+  return (
+    <div>
+      <h1>{ 'hello react' }</h1>
+    </div>
+  );
+}
---
+++ src/webpack/webpack.dev.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    clean: true,
+    path: path.resolve(__dirname, '../../dist'),
+    filename: '[contenthash].[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, '../../src'),
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })]
+};
